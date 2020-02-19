61e3cdc583044acdc72f79617529116259f9e84e
stevezhaoca <steve.zhao@canada.ca>
2020-02-19 10:56:21 -0500

    P/B rt cost recovery v3.1.7 - development

--- dev/src/forms_R1/app/scripts/services/activity-form-filter-service.js
+++ dev/src/forms_R1/app/scripts/services/activity-form-filter-service.js
@@ -53,9 +53,9 @@
                    case 'B02-20160301-038': //Level III
                        newList.push(raList[i]);
                         break;
-                    case 'B02-20160301-046': //MPNC
-                        newList.push(raList[i]);
-                        break;
+                    // case 'B02-20160301-046': //MPNC
+                    //     newList.push(raList[i]);
+                    //     break;
                     case 'B02-20160301-047': //MPNDS
                         newList.push(raList[i]);
                         break;
@@ -287,9 +287,9 @@
                     case 'B02-20160301-038': //Level III
                         newList.push(raList[i]);
                         break;
-                    case 'B02-20160301-046': //MPNC
-                        newList.push(raList[i]);
-                        break;
+                    // case 'B02-20160301-046': //MPNC
+                    //     newList.push(raList[i]);
+                    //     break;
                     case 'B02-20160301-047': //MPNDS
                         newList.push(raList[i]);
                         break;
