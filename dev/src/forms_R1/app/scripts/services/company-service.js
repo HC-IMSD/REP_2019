8c7ab68b02d3d6d8116b96b579bc2ab5a1db78d5
yingxie2018 <ying.xie@canada.ca>
2021-01-21 16:45:09 -0500

    CO: revert and update the country list since the ID should not be changed.

--- dev/src/forms_R1/app/scripts/services/company-service.js
+++ dev/src/forms_R1/app/scripts/services/company-service.js
@@ -89,8 +89,8 @@
                 var defaultAddressRole = {
                     manufacturer: false,
                     mailing: false,
-                    billing: false,
-                    importer: false
+                    billing: false
+                    // importer: false
                 };
                 return (defaultAddressRole);
             },
@@ -110,12 +110,12 @@
                     addressID: 1,
                     companyName: "",
                     businessNumber: "",
-                    importerID: "",
+                    // importerID: "",
                     addressRole: {
                         manufacturer: false,
                         mailing: false,
-                        billing: false,
-                        importer: false
+                        billing: false
+                        // importer: false
                     },
                     street: "",
                     city: "",
@@ -155,8 +155,8 @@
                     phoneExt: "",
                     fax: "",
                     email: "",
-                    routingId: "",
-                    impCompanyName: ""
+                    routingId: ""
+                    // impCompanyName: ""
                 };
                 defaultContact.contactId = this.getNextContactID();
                 return (defaultContact);
@@ -273,12 +273,12 @@
                     address.addressID = adrList[i].address_id;
                     address.companyName = adrList[i].company_name;
                     address.businessNumber = adrList[i].business_number;
-                    address.importerID = adrList[i].importer_id;
+                    // address.importerID = adrList[i].importer_id;
                     address.addressRole = {};
                     address.addressRole.manufacturer = adrList[i].manufacturer === 'Y';
                     address.addressRole.mailing = adrList[i].mailing === 'Y';
                     address.addressRole.billing = adrList[i].billing === 'Y';
-                    address.addressRole.importer = adrList[i].importer === 'Y';
+                    // address.addressRole.importer = adrList[i].importer === 'Y';
                     address.street = adrList[i].company_address_details.street_address;
                     address.city = adrList[i].company_address_details.city;
                     // address.stateList = adrList[i].company_address_details.province_lov;
@@ -340,7 +340,7 @@
                     contact.addressRole.manufacturer = contacts[i].manufacturer === 'Y';
                     contact.addressRole.mailing = contacts[i].mailing === 'Y';
                     contact.addressRole.billing = contacts[i].billing === 'Y';
-                    contact.addressRole.importer = contacts[i].importer === 'Y';
+                    // contact.addressRole.importer = contacts[i].importer === 'Y';
                     contact.addressRole.repPrimary = contacts[i].rep_primary === 'Y';
                     contact.addressRole.repSecondary = contacts[i].rep_secondary === 'Y';
                     //contact.contactRole = contacts[i].company_contact_details.rep_contact_role;
@@ -355,7 +355,7 @@
                     contact.fax = contacts[i].company_contact_details.fax_num;
                     contact.email = contacts[i].company_contact_details.email;
                     contact.routingId = contacts[i].company_contact_details.RoutingID;
-                    contact.impCompanyName = contacts[i].company_contact_details.imp_company_name;
+                    // contact.impCompanyName = contacts[i].company_contact_details.imp_company_name;
                     list.push(contact);
                 }
                 return list;
@@ -380,10 +380,10 @@
                 address.manufacturer = adrList[i].addressRole.manufacturer === true ? 'Y' : 'N';
                 address.mailing = adrList[i].addressRole.mailing === true ? 'Y' : 'N';
                 address.billing = adrList[i].addressRole.billing === true ? 'Y' : 'N';
-                address.importer = adrList[i].addressRole.importer === true ? 'Y' : 'N';
+                // address.importer = adrList[i].addressRole.importer === true ? 'Y' : 'N';
                 address.company_name = adrList[i].companyName;
                 address.business_number = adrList[i].businessNumber;
-                address.importer_id = adrList[i].importerID;
+                // address.importer_id = adrList[i].importerID;
                 address.company_address_details = {};
                 address.company_address_details.street_address = adrList[i].street;
                 address.company_address_details.city = adrList[i].city;
@@ -430,7 +430,7 @@
                 contact.manufacturer = contacts[i].addressRole.manufacturer === true ? 'Y' : 'N';
                 contact.mailing = contacts[i].addressRole.mailing === true ? 'Y' : 'N';
                 contact.billing = contacts[i].addressRole.billing === true ? 'Y' : 'N';
-               contact.importer = contacts[i].addressRole.importer === true ? 'Y' : 'N';
+               // contact.importer = contacts[i].addressRole.importer === true ? 'Y' : 'N';
                 contact.rep_primary = contacts[i].addressRole.repPrimary === true ? 'Y' : 'N';
                 // contact.rep_secondary = contacts[i].addressRole.repSecondary === true ? 'Y' : 'N';
                 //contact.rep_contact_role = contacts[i].addressRole.contactRole === true ? 'Y' : 'N';
@@ -449,7 +449,7 @@
                 contact.company_contact_details.fax_num = contacts[i].fax;
                 contact.company_contact_details.email = contacts[i].email;
                 contact.company_contact_details.RoutingID = contacts[i].routingId;
-                contact.company_contact_details.imp_company_name = contacts[i].impCompanyName;
+                // contact.company_contact_details.imp_company_name = contacts[i].impCompanyName;
                 contactList.push(contact);
             }
         }
